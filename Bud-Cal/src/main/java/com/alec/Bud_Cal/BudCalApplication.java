b0b9a450134cb8257b6ffc85880487e328f5fd26
Alec <AlecBozzo@u.boisestate.edu>
2026-03-14 20:45:22 -0600

    Updated Repo and moved src files

---
+++ Bud-Cal/src/main/java/com/alec/Bud_Cal/BudCalApplication.java
@@ -0,0 +1,13 @@
+package com.alec.Bud_Cal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudCalApplication.class, args);
+	}
+
+}
---
+++ Bud-Cal/src/test/java/com/alec/Bud_Cal/BudCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alec.Bud_Cal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
